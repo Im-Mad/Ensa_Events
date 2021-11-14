6acbe2c2ba2a5bc237076c333e22a580bef0b555
iliassox <84898996+iliassox@users.noreply.github.com>
2021-11-14 16:44:22 +0100

    Add files via upload

---
+++ EventDaoImp.java
@@ -0,0 +1,59 @@
+package ma.ensaevents.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ma.ensaevents.entity.Event;
+
+public class EventDaoImp implements EventDao{
+	
+	@Autowired
+	public SessionFactory sessionFactory;
+
+	@Override
+	public Event FindById(int eventId) {
+		
+		Session session = sessionFactory.getCurrentSession();
+		
+		return session.get(Event.class, eventId);
+	}
+
+	@Override
+	public void Create(Event event) {
+		
+		Session session = sessionFactory.getCurrentSession();
+		
+		session.save(event);
+		
+	}
+
+	@Override
+	public void Delete(Event event) {
+		
+		Session session = sessionFactory.getCurrentSession();
+		
+		Query query = session.createQuery("delete from event where id=eventId");
+		query.setParameter("eventId", event.getId());
+		
+		query.executeUpdate();
+		
+	}
+
+	@Override
+	public List<Event> findAllEvents() {
+		Session session = sessionFactory.getCurrentSession();
+		
+		List<Event> events = session.createQuery("select * from event ").getResultList();
+		
+		return events;
+	}
+	
+	
+	
+	
+
+}
