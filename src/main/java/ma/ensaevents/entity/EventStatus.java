ae157070d592c1e8d552e19552dafbded3281e52
Rachid <rachid-tcs2000@gmail.fr>
2021-12-20 15:38:13 +0100

    Status

---
+++ src/main/java/ma/ensaevents/entity/EventStatus.java
@@ -0,0 +1,29 @@
+package ma.ensaevents.entity;
+
+public enum EventStatus {
+
+    ONGOING("ongoing", "yellow"),
+    ENDED("ended", "red"),
+    UPCOMING("upcoming", "green");
+
+    private final String label;
+    private final String color;
+
+    EventStatus(String label, String color) {
+        this.label = label;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return  label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
